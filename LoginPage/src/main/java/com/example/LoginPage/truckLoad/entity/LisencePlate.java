001a80ce49d1225718712b8874ea05811bb10582
unknown <sanwalkanika9@gmail.com>
2024-10-24 11:30:47 +0530

    new changes

--- LoginPage/src/main/java/com/example/LoginPage/truckLoad/entity/LisencePlate.java
+++ LoginPage/src/main/java/com/example/LoginPage/truckLoad/entity/LisencePlate.java
@@ -1,5 +1,6 @@
 package com.example.LoginPage.truckLoad.entity;
 
+import com.example.LoginPage.oms.entity.FepOrdersInfo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +8,8 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
--- LoginPage/src/main/java/com/example/LoginPage/user/entity/Permission.java
+++ LoginPage/src/main/java/com/example/LoginPage/user/entity/Permission.java
@@ -1,6 +1,8 @@
 package com.example.LoginPage.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 //@Getter
